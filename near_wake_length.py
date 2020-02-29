d2685fa8683b9a8d3620e90df2d06e19b2d7b732
peterdougstuart <peter.stuart@gmail.com>
2020-02-29 22:57:59 +0000

    Added balance of code to calculate near wake length

--- near_wake_length.py
+++ near_wake_length.py
@@ -1,5 +1,7 @@
+import math
 
-def ambient_turbulence_wake_erosion_rate(turbulence):
+
+def calculate_ambient_turbulence_wake_erosion_rate(turbulence):
     
     # Equate 19 (from p440)
     # An Experimental analysis of wind turbine wakes, P.E.J. Vermeulen, Wind Energy Systems August 26-29, 1980.
@@ -8,3 +10,71 @@ def ambient_turbulence_wake_erosion_rate(turbulence):
         return 5.0 * turbulence
     else:
         return 2.5 * turbulence + 0.05
+
+
+def calculate_angular_velocity(rpm):
+    return rpm * (2.0 * math.pi / 60.0)
+
+
+def calculate_flow_field_ratio(thrust_coefficient):
+    # here flow field ratio is the ratio of the unpeturbed velocity to the (constant) velocity in the potential core
+    # ref Equations 7 and 8 in Vermeulen 1980
+    # m = Uinf / U2
+
+    if thrust_coefficient > 0.8888:
+        return 3.0
+    else:
+        return 1.0 / math.sqrt(1.0 - thrust_coefficient)
+
+
+def calculate_mechanical_turbulence_wake_erosion_rate(number_of_blades, tip_speed_ratio):
+    return 0.012 * number_of_blades * tip_speed_ratio
+
+def calculate_n(flow_field_ratio):
+
+    A = 0.214
+    B = 0.144
+    C = 0.134
+    D = 0.124
+
+    c1 = math.sqrt(A + B * flow_field_ratio)
+    c2 = math.sqrt(C + D * flow_field_ratio)
+
+    return c1 * (1.0 - c2) / ((1.0 - c1) * c2)
+
+
+def calculate_radius_of_inviscid_expanded_rotor_disk(diameter, flow_field_ratio):
+
+    # ref Equation 9 in Vermeulen 1980
+    radius = 0.5 * diameter
+
+    return radius * math.sqrt((flow_field_ratio + 1.0) / 2.0)
+
+
+def calculate_shear_generated_turbulence_wake_erosion_rate(flow_field_ratio):
+    return (1.0 - flow_field_ratio) * math.sqrt(1.49 + flow_field_ratio) / (9.76 * (1.0 + flow_field_ratio))
+
+
+def calculate_tip_speed_ratio(angular_velocity, radius, velocity):
+    return (angular_velocity * radius) / velocity
+
+
+def calculate_total_wake_erosion_rate(ambient_turbulence_wake_erosion_rate, shear_generated_turbulence_wake_erosion_rate, mechanical_turbulence_wake_erosion_rate):
+    return math.sqrt(ambient_turbulence_wake_erosion_rate ** 2.0 + shear_generated_turbulence_wake_erosion_rate ** 2.0 + mechanical_turbulence_wake_erosion_rate ** 2.0)
+
+
+def calculate_radius(diameter):
+    return diameter * 0.5
+
+
+def calculate_near_wake_length(diameter, thrust_coefficient, rpm, number_of_blades, velocity, turbulence):
+
+    # The near wake length is the distance after which the potential core is completely eroded
+
+    flow_field_ratio = calculate_flow_field_ratio(thrust_coefficient)
+    tip_speed_ratio = calculate_tip_speed_ratio(calculate_angular_velocity(rpm), calculate_radius(diameter), velocity)
+
+    return calculate_n(flow_field_ratio) * calculate_radius_of_inviscid_expanded_rotor_disk(diameter, flow_field_ratio) \
+           / calculate_total_wake_erosion_rate(calculate_ambient_turbulence_wake_erosion_rate(turbulence),
+                                               calculate_shear_generated_turbulence_wake_erosion_rate(flow_field_ratio),
+                                               calculate_mechanical_turbulence_wake_erosion_rate(number_of_blades, tip_speed_ratio))
--- tests/test_near_wake_length.py
+++ tests/test_near_wake_length.py
@@ -1,9 +1,40 @@
 import pytest
 
-from near_wake_length import ambient_turbulence_wake_erosion_rate
+from near_wake_length import calculate_ambient_turbulence_wake_erosion_rate
+from near_wake_length import calculate_angular_velocity
+from near_wake_length import calculate_flow_field_ratio
+from near_wake_length import calculate_mechanical_turbulence_wake_erosion_rate
+from near_wake_length import calculate_n
+from near_wake_length import calculate_radius_of_inviscid_expanded_rotor_disk
+from near_wake_length import calculate_near_wake_length
 
-def test_ambient_turbulence_wake_erosion_rate():
+def test_calculate_ambient_turbulence_wake_erosion_rate():
 	ambientTurbuluence = 0.1
-	assert ambient_turbulence_wake_erosion_rate(ambientTurbuluence) == (2.5 * ambientTurbuluence + 0.05)
+	assert calculate_ambient_turbulence_wake_erosion_rate(ambientTurbuluence) == (2.5 * ambientTurbuluence + 0.05)
 
-#to run this write 'py.test test.py' in the command line
+def test_calculate_angular_velocity():
+	assert calculate_angular_velocity(0.0) == 0.0
+
+def test_calculate_flow_field_ratio():
+	assert calculate_flow_field_ratio(1.0) == 3.0
+
+def test_calculate_mechanical_turbulence_wake_erosion_rate():
+	assert calculate_mechanical_turbulence_wake_erosion_rate(3, 0.0) == 0.0
+
+def test_calculate_n():
+	pass #todo
+
+def test_calculate_radius_of_inviscid_expanded_rotor_disk():
+	pass #todo
+
+def test_calculate_shear_generated_turbulence_wake_erosion_rate():
+	pass #todo
+
+def test_calculate_tip_speed_ratio():
+	pass #todo
+
+def test_calculate_total_wake_erosion_rate():
+	pass #todo
+
+def testcalculate__near_wake_length():
+	assert calculate_near_wake_length(76, 0.7, 15, 3, 10, 0.15) == pytest.approx(140.0160145)
