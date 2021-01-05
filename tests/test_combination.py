b7da427436873b613a8b73bbe77850de3ecb110b
Tom Young <33696649+tomyoung1@users.noreply.github.com>
2021-01-05 12:40:25 +0000

    Further test fix

--- tests/test_combination.py
+++ tests/test_combination.py
@@ -9,10 +9,10 @@ def test_velicity_deficit_combination():
 
     combiner.add(value=0.1, normalised_distance_upwind=3.0, normalised_lateral_distance=0.0)
 
-    assert combiner.combined_value == 0.1
+    assert combiner.combined_value() == 0.1
 
     combiner.add(value=0.05, normalised_distance_upwind=7.0, normalised_lateral_distance=0.0)
 
     assert combiner.closest_normalised_distance_upwind == 3.0
 
-    assert combiner.combined_value == 0.1
+    assert combiner.combined_value() == 0.1
