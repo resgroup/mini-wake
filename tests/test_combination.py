1d0e077a57bd8b6f1da0ec0f246f0d9c4e495ed8
Tom Young <33696649+tomyoung1@users.noreply.github.com>
2021-01-11 16:15:40 +0000

    Wake Combination Options (#8)
    * Wake Combination Options
    
    Added wake combination options. Also replaced the @property code with methods to give a small speed improvement
    
    * Update test_combination.py
    
    * Fix combination call to make tests work
    
    * Further test fix
    
    * Improved coverage of combination test

--- tests/test_combination.py
+++ tests/test_combination.py
@@ -1,18 +1,29 @@
 import pytest
 
-from miniwake.combination import VelocityDeficitCombiner
+from miniwake.combination import RSSMaxAutoVelocityDeficitCombiner, WeightedAverageRSSLinearVelocityDeficitCombiner, StraightAverageRSSLinearVelocityDeficitCombiner
 
 
 def test_velicity_deficit_combination():
 
-    combiner = VelocityDeficitCombiner()
+    combiner = RSSMaxAutoVelocityDeficitCombiner()
+    combiner1 = WeightedAverageRSSLinearVelocityDeficitCombiner()
+    combiner2 = StraightAverageRSSLinearVelocityDeficitCombiner()
 
     combiner.add(value=0.1, normalised_distance_upwind=3.0, normalised_lateral_distance=0.0)
+    combiner1.add(value=0.1, normalised_distance_upwind=3.0, normalised_lateral_distance=0.0)
+    combiner2.add(value=0.1, normalised_distance_upwind=3.0, normalised_lateral_distance=0.0)
 
-    assert combiner.combined_value == 0.1
+    assert combiner.combined_value() == 0.1
+    assert abs(combiner1.combined_value() - 0.1) < 1E-6
+    assert abs(combiner2.combined_value() - 0.1) < 1E-6
 
     combiner.add(value=0.05, normalised_distance_upwind=7.0, normalised_lateral_distance=0.0)
+    combiner1.add(value=0.05, normalised_distance_upwind=7.0, normalised_lateral_distance=0.0)
+    combiner2.add(value=0.05, normalised_distance_upwind=7.0, normalised_lateral_distance=0.0)
 
     assert combiner.closest_normalised_distance_upwind == 3.0
 
-    assert combiner.combined_value == 0.1
+    assert combiner.combined_value() == 0.1
+    assert abs(combiner1.combined_value() - 0.1232623) < 1E-6
+    assert abs(combiner2.combined_value() - 0.1309017) < 1E-6
+
